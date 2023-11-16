e23c87288906ae2cd9c7b9d4e22ca1a2e8819293
Robertjo97 <robertjo97@gmail.com>
2023-11-16 14:20:14 -0800

    database created

--- create_database.php
+++ create_database.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $servername = "localhost";
 $username = "AdminLab11";
 $password = "4VPnroTOC6wOU3mn";
@@ -9,18 +9,12 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error){
     die("Connection failed: " . $conn->error);
 }
-$sql = "CREATE DATABASE myDB";
+$sql = "CREATE DATABASE NationalParks";
 if($conn->query($sql) === TRUE){
     echo 'database created successfully';
 }
 else {
     echo "Error: " . $conn->error; 
 }
-
-// $sql = "CREATE TABLE Parks (
-//     id INT AUTO_INCREMENT PRIMARY KEY,
-    
-
-
 $conn->close();
 ?>
