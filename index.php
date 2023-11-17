261914d494ecfdab3f3d904d4679ffa5b047d689
Robertjo97 <robertjo97@gmail.com>
2023-11-16 16:14:33 -0800

    Finish index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php 
-$rawData = file_get_contents('./data.json');
-$data = json_decode($rawData);
-$size = count($data);
-$park = null;
+$servername = "localhost";
+$username = "AdminLab11";
+$password = "4VPnroTOC6wOU3mn";
+$dbname = 'NationalParks';
+$index = null;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Error " . $conn->connect_error);
+}
+
+$result = $conn->query("SELECT COUNT(*) AS count FROM Parks");
+$row = $result->fetch_assoc();
+$size = $row['count'];
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(isset($_POST['index'])){
@@ -13,11 +24,16 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         else if($index == -1){
             $index = $size - 1;
         }
-        $park = $data[$index];
+        $sql = "SELECT * FROM Parks LIMIT 1 OFFSET $index";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $park = array ('name' => $row['name'], 'location' => $row['location'], 'yearEstablished' => $row['yearEstablished'], 'freeEntry' => $row['freeEntry'], 'biome' => $row['biome'], 'img' => $row['imgURL']);
         $response = array('park' => $park, 'size' => $size, 'position' => $index + 1);
         echo json_encode($response);
     }
 }
+
+$conn->close();
 ?>
 
 
